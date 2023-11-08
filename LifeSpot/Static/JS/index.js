f8e8801fbc30e1b514c7e9d19232f2e60321f68f
Name <fenderpugach@gmail.com>
2023-11-09 00:15:50 +0700

    Задание 29.5.5. решено

---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,48 @@
+﻿
+
+let session = new Map();
+function handleSession() {
+    // Сохраним UserAgent
+    session.set("startDate", new Date().toLocaleString())
+    // Сохраним UserAgent
+    session.set("userAgent", window.navigator.userAgent)
+}
+
+// Запросим возраст пользователя и тоже сохраним
+function checkAge() {
+    session.set("age", prompt("Пожалуйста, введите ваш возраст"))
+
+    // Проверка на возраст и сохранение сессии
+    if (session.get("age") >= 18) {
+        let startDate = new Date().toLocaleString();
+
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
+        session.set("startDate", startDate)
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+}
+window.onload = handleSession();
+
+function filterContent(inputParseFunction) {
+    let elements = document.getElementsByClassName('video-container');
+
+    for (let i = 0; i <= elements.length; i++) {
+        let videoText = elements[i].querySelector(".video-title").innerText;
+        if (!videoText.toLowerCase().includes(inputParseFunction() /* Захват переменной теперь происходит с помощью замыкания */.toLowerCase())) {
+            elements[i].style.display = 'none';
+        } else {
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
+let sessionLog = function logSession(session) {
+    // Вывод в консоль
+    for (let result of session) {
+        console.log(result)
+    }
+}
+
+
